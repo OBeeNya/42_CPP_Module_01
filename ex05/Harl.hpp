487a3471ccfbfeab3a0ca04bd593c6824acf52e7
Benjamin AUBIGNAT <baubigna@e2r6p1.clusters.42paris.fr>
2022-09-28 16:27:42 +0200

    ex05

---
+++ ex05/Harl.hpp
@@ -0,0 +1,32 @@
+#ifndef HARL_HPP
+# define HARL_HPP
+
+# include <string>
+# include <iostream>
+
+class Harl
+{
+
+	public:
+
+	Harl(void);
+	~Harl(void);
+
+	void	complain(std::string level);
+
+	private:
+
+	void	_debug(void);
+	void	_info(void);
+	void	_warning(void);
+	void	_error(void);
+
+};
+
+struct	func_ptr
+{
+	void	(Harl::*f)(void);
+};
+
+
+#endif
